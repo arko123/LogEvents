4995e6e22de384068f1f354ada4377c6428856f1
LenovoY580_Wojtek <LenovoY580_Wojtek@LenovoY580>
2017-04-27 18:56:35 +0200

    LOG ANALYZER v2

--- src/main/java/analize/LogAnalizer.java
+++ src/main/java/analize/LogAnalizer.java
@@ -1,8 +1,15 @@
 package analize;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import com.sun.mail.handlers.message_rfc822;
 
 import mail.MailProperties;
 import mail.MailSender;
@@ -17,12 +24,34 @@ public class LogAnalizer {
 	}
 	
 	
-	public void run(){
+	public void run() throws IOException{
 		File directory = new File(logsLocation);
 		List<File> files = Arrays.asList(directory);
 		
+		boolean sendEmail = false;
+		String title = "[LOG ANALYZER] Raport";
 		
 		
+		StringBuffer messageBuffer = new StringBuffer("");
+		
+		
+		for(File file: files){
+			BufferedReader br=new BufferedReader(new FileReader(file));
+			String line;
+			
+			while((line=br.readLine())!=null){
+				if(true){
+					sendEmail = true;
+					messageBuffer.append("\n"+line);
+				}
+			}
+			br.close();
+		}
+		
+		
+		
+		if(sendEmail)
+			sendEmail(title,messageBuffer.toString());
 		
 		clearFiles(files);
 	}
