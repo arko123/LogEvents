bdc1fe05d02ce39d0c007a1f390f641473969fa7
LenovoY580_Wojtek <LenovoY580_Wojtek@LenovoY580>
2017-05-28 23:47:59 +0200

    Junit GetLogsOverHttpTest

--- src/test/java/download/GetLogsOverHttpTest.java
+++ src/test/java/download/GetLogsOverHttpTest.java
@@ -1,19 +1,48 @@
 package download;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class GetLogsOverHttpTest {
+	String dirURL ;
+	String fileName ;
+	String saveDir;
+	GetLogsOverHttp getLogs;
+	
+	@Before
+	public  void init(String[] args) throws IOException, InterruptedException {
+		 dirURL = "http://student.agh.edu.pl/~wbachta/ZTW/";
+		 fileName = "Start.txt";
+		 saveDir ="C:\\Users\\LenovoY580_Wojtek\\git\\LogEvents\\testFiles\\to\\";
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		String fileURL = "http://student.agh.edu.pl/~wbachta/ZTW/Start.txt";
-		String saveDir ="C:\\Users\\LenovoY580_Wojtek\\git\\LogEvents\\testFiles\\to\\";
-
-		GetLogsOverHttp getLogs = new GetLogsOverHttp(fileURL, saveDir);
-		getLogs.getLatestLogs();
+		 getLogs = new GetLogsOverHttp(saveDir+fileName, saveDir);
 		
-		Thread.sleep(60000);
+	}
+	
+	@Test
+	public  void getLocalLogsTest()  {
 		
-		getLogs.getLatestLogs();
+		Scanner sc;
+		try {
+			getLogs.getLatestLogs();
+			sc = new Scanner(new File(saveDir+fileName));
+			if(sc.hasNext()){
+				 String line = sc.nextLine(); 
+				 assertTrue( true);
+			}
+			
+		} catch (IOException e) {
+			fail();
+		}
+		
 	}
 
 }
